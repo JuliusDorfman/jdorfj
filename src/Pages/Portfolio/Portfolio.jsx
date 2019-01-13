9566992392342c042ab65ce0384784934bf16166
JuliusDorfman <juliusdorfman@gmail.com>
2019-01-13 11:39:20 -0800

    sincere welcome message update. No more Lorem ipsum!

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -84,7 +84,7 @@ export default class Portfolio extends Component {
               You've found my portfolio! I'm Julius Dorfman. I engineer ambitious applications and design them with a minimalist aesthetic. 
               <br />
               <br />
-              This website is where I publish my works; an expression of my creativity through design, code, and writing. 
+              This website is where I publish my works; an expression of my creativity through design, code, and writing. This site will hold the bookmarks of my life in: projects, articles, and career history. While you're here, thank you for being a part of it.
               </p>
           </div>
         </section>
